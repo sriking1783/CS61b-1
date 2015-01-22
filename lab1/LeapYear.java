d4e4abd787f7d1b150b848f4ccc21e7bea8c2f00
Aditya Iyengar <adityaiyengar@Adityas-MacBook-Pro.local>
2015-01-21 22:06:03 -0800

    Completed LeapYear.java

---
+++ lab1/LeapYear.java
@@ -0,0 +1,18 @@
+public class LeapYear {
+	public static void main (String[] args) {
+		int year = 1993;
+		if (isLeapYear(year)) {
+			System.out.println(year + " is a leap year.");
+		}
+		else {
+			System.out.println(year + " is not a leap year.");
+		}
+	}
+
+	public static boolean isLeapYear (int year) {
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+			return true;
+		}
+		return false;
+	}
+}
