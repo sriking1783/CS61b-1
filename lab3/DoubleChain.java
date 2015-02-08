1183fb07723d7b803ca2758f31983c50c0a16e43
Aditya Iyengar <adityaiyengar@Adityas-MacBook-Pro.local>
2015-02-08 00:52:33 -0800

    Finally Finished

--- lab3/DoubleChain.java
+++ lab3/DoubleChain.java
@@ -4,7 +4,6 @@ public class DoubleChain {
 	private DNode head;
 	
 	public DoubleChain(double val) {
-<<<<<<< HEAD
 		head = new DNode(val);
 	}
 
@@ -14,60 +13,39 @@ public class DoubleChain {
 			temp = temp.next;
 		}
 		return temp;
-=======
-		/* your code here. */
-		head = null; 
-	}
-
-	public DNode getFront() {
-		return head;
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 	}
 
 	/** Returns the last item in the DoubleChain. */		
 	public DNode getBack() {
-<<<<<<< HEAD
 		DNode temp = head;
 		while (temp.prev != null) {
 			temp = temp.prev;
 		}
 		return temp;
-=======
-		/* your code here */
-		return null;
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 	}
 	
 	/** Adds D to the front of the DoubleChain. */	
 	public void insertFront(double d) {
-<<<<<<< HEAD
 		DNode temp = head;
 		while (temp.next != null) {
 			temp = temp.next;
 		}
 		temp.next = new DNode(temp, d, null);
-=======
-		/* your code here */
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 	}
 	
 	/** Adds D to the back of the DoubleChain. */	
 	public void insertBack(double d) {
-<<<<<<< HEAD
+
 		DNode temp = head;
 		while (temp.prev != null) {
 			temp = temp.prev;
 		}
 		temp.prev = new DNode(null, d, temp);
-=======
-		/* your code here */
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 	}
 	
 	/** Removes the last item in the DoubleChain and returns it. 
 	  * This is an extra challenge problem. */
 	public DNode deleteBack() {
-<<<<<<< HEAD
 		if (head != null) {
 			DNode rv = this.getBack();
 			DNode temp = this.getBack().next;
@@ -77,16 +55,12 @@ public class DoubleChain {
 			head = temp;
 			return rv;
 		}
-=======
-		/* your code here */
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 		return null;
 	}
 	
 	/** Returns a string representation of the DoubleChain. 
 	  * This is an extra challenge problem. */
 	public String toString() {
-<<<<<<< HEAD
 		String returnObj = "";
 		DNode temp = this.getBack();
 		while (temp.next != null)	{
@@ -96,10 +70,6 @@ public class DoubleChain {
 
 		returnObj = returnObj + this.getFront().val;
 		return returnObj;
-=======
-		/* your code here */		
-		return null;
->>>>>>> 090c93dc4065bbe7fed1839581959c1e0f32df5f
 	}
 
 	public static class DNode {
