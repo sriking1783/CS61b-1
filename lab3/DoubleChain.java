593c5dc79d61a1cf75533917838a3c8ebad9d22b
Aditya Iyengar <adityaiyengar@Adityas-MacBook-Pro.local>
2015-02-08 00:37:16 -0800

    Completed Lab #3

---
+++ lab3/DoubleChain.java
@@ -0,0 +1,90 @@
+
+public class DoubleChain {
+	
+	private DNode head;
+	
+	public DoubleChain(double val) {
+		head = new DNode(val);
+	}
+
+	public DNode getFront() {
+		DNode temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	/** Returns the last item in the DoubleChain. */		
+	public DNode getBack() {
+		DNode temp = head;
+		while (temp.prev != null) {
+			temp = temp.prev;
+		}
+		return temp;
+	}
+	
+	/** Adds D to the front of the DoubleChain. */	
+	public void insertFront(double d) {
+		DNode temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = new DNode(temp, d, null);
+	}
+	
+	/** Adds D to the back of the DoubleChain. */	
+	public void insertBack(double d) {
+		DNode temp = head;
+		while (temp.prev != null) {
+			temp = temp.prev;
+		}
+		temp.prev = new DNode(null, d, temp);
+	}
+	
+	/** Removes the last item in the DoubleChain and returns it. 
+	  * This is an extra challenge problem. */
+	public DNode deleteBack() {
+		if (head != null) {
+			DNode rv = this.getBack();
+			DNode temp = this.getBack().next;
+			if (temp != null) {
+				temp.prev = null;
+			}
+			head = temp;
+			return rv;
+		}
+		return null;
+	}
+	
+	/** Returns a string representation of the DoubleChain. 
+	  * This is an extra challenge problem. */
+	public String toString() {
+		String returnObj = "";
+		DNode temp = this.getBack();
+		while (temp.next != null)	{
+			returnObj = returnObj + temp.val + ", ";
+			temp = temp.next;
+		}
+
+		returnObj = returnObj + this.getFront().val;
+		return returnObj;
+	}
+
+	public static class DNode {
+		public DNode prev;
+		public DNode next;
+		public double val;
+		
+		private DNode(double val) {
+			this(null, val, null);
+		}
+		
+		private DNode(DNode prev, double val, DNode next) {
+			this.prev = prev;
+			this.val = val;
+			this.next =next;
+		}
+	}
+	
+}
---
+++ lab3/TestDoubleChain.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Perform tests of the DoubleChain class
+ */
+
+public class TestDoubleChain {
+
+    /** Tests the constructor of DoubleChain */
+    @Test
+    public void testConstructor() {
+        DoubleChain d = new DoubleChain(5);
+        assertEquals(5,d.getFront().val, 1e-6);
+        assertEquals(null, d.getFront().prev);
+        assertEquals(null, d.getFront().next);
+    }
+
+    /** Tests some basic DoubleChain operations. */
+    @Test
+    public void testBasicOperations() {
+        DoubleChain d = new DoubleChain(5);
+        assertEquals(5, d.getFront().val, 1e-11);
+        assertEquals(5, d.getBack().val, 1e-11);
+
+        d.insertFront(2);
+        d.insertFront(1);
+        d.insertBack(7);
+        d.insertBack(8);
+        assertEquals(1, d.getFront().val, 1e-11);
+        assertEquals(8, d.getBack().val, 1e-11);
+
+        assertEquals(8, d.deleteBack().val, 1e-11);
+        assertEquals(7, d.getBack().val, 1e-11);
+
+        DoubleChain x = new DoubleChain(5);
+        assertEquals(5, x.deleteBack().val, 1e-11);
+        assertEquals(null, x.deleteBack());        
+    }
+
+    @Test
+    public void testToString() {
+        DoubleChain d = new DoubleChain(3.3);
+        d.insertFront(4.6);
+        d.insertFront(5.2);
+        d.insertBack(2.1);
+        d.insertBack(1.0);
+        assertEquals("1.0, 2.1, 3.3, 4.6, 5.2", d.toString());
+    }
+
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestDoubleChain.class);
+    }
+}
