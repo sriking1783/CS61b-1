4bcbe13bde50c0ef4995f1235c13659765cdd390
Aditya Iyengar <adityaiyengar@Adityas-MacBook-Pro.local>
2015-02-12 03:17:49 -0800

    dd

--- proj0/Piece.java
+++ proj0/Piece.java
@@ -79,13 +79,13 @@ public class Piece {
 		}
 		if (this.isBomb()) {
 			Piece thisBomb = board.remove(x, y);
-			for (int i = x - 1; i < 3; i += 2) {
-				for (int j = y - 1; j < 3; i += 2) {
-					if (board.pieceAt(i, j) != null && !board.pieceAt(i, j).isShield()) {
-						board.remove(i, j).beenCaptured = true;
-					}
-				}
-			}
+			// for (int i = x - 1; i < 3; i += 2) {
+			// 	for (int j = y - 1; j < 3; i += 2) {
+			// 		if (board.pieceAt(i, j) != null && !board.pieceAt(i, j).isShield()) {
+			// 			board.remove(i, j).beenCaptured = true;
+			// 		}
+			// 	}
+			// }
 		}
 		xPos = x;
 		yPos = y;
