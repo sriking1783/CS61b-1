e4caff34491ef1d48144da30389531b59cc5b413
Aditya Iyengar <adityaiyengar@Adityas-MacBook-Pro.local>
2015-02-12 03:26:51 -0800

    tiny bud fixed

--- proj0/Piece.java
+++ proj0/Piece.java
@@ -80,7 +80,7 @@ public class Piece {
 		if (this.isBomb()) {
 			Piece thisBomb = board.remove(x, y);
 			for (int i = x - 1; i < x + 1; i += 2) {
-				for (int j = y - 1; x + 1; i += 2) {
+				for (int j = y - 1; y + 1; i += 2) {
 					if (board.pieceAt(i, j) != null && !board.pieceAt(i, j).isShield()) {
 						board.remove(i, j).beenCaptured = true;
 					}
